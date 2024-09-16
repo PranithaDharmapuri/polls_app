d9de7bb8bf15152c95c39e2421bf2cf93f140f4c
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2024-09-16 19:18:02 +0530

    added html file to templates/polls dir

--- polls/views.py
+++ polls/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world.This is my polls app.")
+    latest_question_list = Questions.objects.order_by("-pub_date")[:5]
+    output = ", ".join([q.question_text for q in latest_question_list])
+    return HttpResponse(output)
 
 def detail(request , question_id):
     return HttpResponse("You are looking at question %s."% question_id)
