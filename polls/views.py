0215e149368d8deaa5030e8b13d92467817d1685
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2024-09-19 19:29:52 +0530

    reworked on views by undoing the generic views

--- polls/views.py
+++ polls/views.py
@@ -1,27 +1,25 @@
-from django.db.models import F
-from django.http import HttpResponse, HttpResponseRedirect
+from django.http import Http404
+from django.http import  HttpResponseRedirect
 from django.shortcuts import get_object_or_404,render
 from django.urls import reverse
 from django.views import generic
 
 from .models import Choice,Questions
 
+def index(request):
+    latest_question_list=Questions.objects.order_by('-pub_date')[:5]
+    context={
+        'latest_question_list':latest_question_list
+    }
+    return render(request,"polls/index.html",context)
+ 
+def detail(request,question_id):
+    question=get_object_or_404(Questions,pk=question_id)
+    return render(request,"polls/detail.html",{'question':question})
 
-class IndexView(generic.ListView):
-    template_name= "polls/index.html"
-    context_object_name="latest_question_list"
-
-    def get_queryset(self):
-        """Return the last five published questions."""
-        return Questions.objects.order_by("-pub_date")[:5]
-    
-class DetailView(generic.DetailView):
-    model=Questions
-    template_name="polls/detail.html"
-
-class ResultsView(generic.DetailView):
-    model=Questions
-    template_name="polls/results.html"
+def results(request,question_id):
+    response="you are loking at results of question %s."
+    return render(request,"polls/results.html",{'response':response})
 
 def vote(request, question_id):
     question = get_object_or_404(Questions, pk=question_id)
