fdaae21a0f9830befc464802ada2948b1834bd50
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2024-09-21 16:12:38 +0530

    all good till now only generic views are not added and vote is not working

--- polls/views.py
+++ polls/views.py
@@ -1,8 +1,8 @@
-from django.http import Http404
-from django.http import  HttpResponse,HttpResponseRedirect
+from django.http import HttpResponseRedirect
 from django.shortcuts import render,get_object_or_404
 from django.urls import reverse
-from django.views import generic
+#from django.views import generic
+from django.db.models import F
 
 from .models import Choice,Questions
 
@@ -26,16 +26,16 @@ def vote(request, question_id):
     try:
         selected_choice = question.choice_set.get(pk=request.POST["choice"])
     except (KeyError, Choice.DoesNotExist):
-        return render(
-            request, "polls/detail.html",   
-            {
+        context={
                 "question":question,
                 "error_message": "You didn't select a choice.",
-            },
+                }
+        return render(
+            request, "polls/detail.html",context
         )
     else:
         selected_choice.votes=F("votes") + 1
         selected_choice.save()
 
-        return HttpResponseRedirect(reverse("polls:results",args=(question.id)))
+        return HttpResponseRedirect(reverse("polls:results",args=(question.id,)))
     
