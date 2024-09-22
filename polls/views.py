9398605d1d273fd388bc5dd1926216a6cd736e05
Pranitha Dharmapuri <pranithadharmapuri29@gmail.com>
2024-09-22 12:22:02 +0530

    committed

--- polls/views.py
+++ polls/views.py
@@ -21,17 +21,17 @@ def results(request,question_id):
     question=get_object_or_404(Questions,pk=question_id)
     return render(request,'polls/results.html',{'question': question})
 
-def vote(request, question_id):
+def vote(request,question_id):
     question = get_object_or_404(Questions, pk=question_id)
     try:
-        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
     except (KeyError, Choice.DoesNotExist):
-        context={
+        return render(
+            request, "polls/detail.html",   
+            {
                 "question":question,
                 "error_message": "You didn't select a choice.",
-                }
-        return render(
-            request, "polls/detail.html",context
+            },
         )
     else:
         selected_choice.votes=F("votes") + 1
